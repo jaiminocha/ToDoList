7590d8f52aa1b72781dbd8dbc9f5b846ae1c2e0c
Jai Minocha <edit>
2021-10-22 12:08:54 +0530

    Changed a little gitignore, procfile and update procs

--- app.js
+++ app.js
@@ -169,10 +169,9 @@ app.post("/delete", function(req, res) {
 
     if (listName === day) {
         Item.findByIdAndRemove(checkedItemId, function(err) {
-            if (!err) {
-                console.log("Successfully deleted checked item.");
-                res.redirect("/");
-            }
+            console.log("Successfully deleted checked item.");
+            res.redirect("/");
+
         });
     } else {
         List.findOneAndUpdate({ name: listName }, { $pull: { items: { _id: checkedItemId } } }, function(err, foundList) {
